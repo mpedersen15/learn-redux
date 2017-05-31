c2d48796edc6268eaba67076b06db76f7428a4df
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-05-30 20:11:14 -0600

    Added actions for adding and removing todos

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -8,6 +8,8 @@ var defaultState = {
   todos: []
 };
 
+var nextTodoId = 1;
+
 var reducer = (state = defaultState , action) => {
   switch(action.type){
     case 'CHANGE_SEARCH_TEXT':
@@ -15,16 +17,42 @@ var reducer = (state = defaultState , action) => {
         ...state,
         searchText: action.searchText
       };
+    case 'ADD_TODO':
+      // add a todo to state
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: nextTodoId++,
+            text: action.text
+          }
+        ]
+      }
+    case 'REMOVE_TODO':
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.id)
+      }
     default:
       return state;
   }
 };
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes to store
+
+var unsubscribe = store.subscribe( () => {
+  var state = store.getState();
+  console.log('state changed', state);
+  document.getElementById('app').innerHTML = state.searchText;
+});
+// unsubscribe();
 
 var currentState = store.getState();
-
-console.log('currentState', currentState);
 
 var action = {
   type: 'CHANGE_SEARCH_TEXT',
@@ -33,4 +61,27 @@ var action = {
 
 store.dispatch(action);
 
-console.log('should have new search text', store.getState());
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'New new search text'
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'A very new todo'
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'A super new todo'
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'A third new todo'
+});
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  id: 1
+});
