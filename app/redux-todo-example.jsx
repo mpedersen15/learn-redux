3d98151b31134f85b9a97695b8c3f8b8b23f3218
mpedersen15 <mpedersen@mindrocketdesign.com>
2017-06-01 10:52:43 -0600

    added separate reducers and action generators for todo example

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -2,15 +2,92 @@ var redux = require('redux');
 
 console.log('starting redux 2 example');
 
-var defaultState = {
+/*var defaultState = {
   searchText: '',
   showCompleted: false,
   todos: []
-};
+};*/
 
+
+
+
+// searchText handlers
+// ------------------------------
+var searchTextReducer = (state = '', action) => {
+  switch(action.type){
+    case 'CHANGE_SEARCH_TEXT':
+      return action.searchText;
+    default:
+      return state;
+  }
+}
+
+var changeSearchText = (searchText) => {
+  return {
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText
+  };
+}
+
+// showCompleted handlers
+// ------------------------------
+var showCompletedReducer = (state = false, action) => {
+  switch(action.type){
+    case 'CHANGE_SHOW_COMPLETED':
+      return action.showCompleted;
+    default:
+      return state;
+  }
+}
+
+var changeShowCompleted = (showCompleted) => {
+  return {
+    type: 'CHANGE_SHOW_COMPLETED',
+    showCompleted
+  };
+}
+
+// todos handlers
+// ------------------------------
 var nextTodoId = 1;
+var todosReducer = (state = [], action) => {
+  switch(action.type){
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: nextTodoId++,
+          text: action.text
+        }
+      ];
+    case 'REMOVE_TODO':
+      return state.filter((todo) => todo.id !== action.id)
+    default:
+      return state;
+  }
+}
 
-var reducer = (state = defaultState , action) => {
+var addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    text
+  };
+}
+
+var removeTodo = (id) => {
+  return {
+    type: 'REMOVE_TODO',
+    id
+  };
+}
+
+var reducer = redux.combineReducers({
+  searchText: searchTextReducer,
+  showCompleted: showCompletedReducer,
+  todos: todosReducer
+});
+
+/*var reducer = (state = defaultState , action) => {
   switch(action.type){
     case 'CHANGE_SEARCH_TEXT':
       return {
@@ -37,7 +114,7 @@ var reducer = (state = defaultState , action) => {
     default:
       return state;
   }
-};
+};*/
 
 var store = redux.createStore(reducer, redux.compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -50,6 +127,7 @@ var unsubscribe = store.subscribe( () => {
   console.log('state changed', state);
   document.getElementById('app').innerHTML = state.searchText;
 });
+
 // unsubscribe();
 
 var currentState = store.getState();
@@ -61,27 +139,14 @@ var action = {
 
 store.dispatch(action);
 
-store.dispatch({
-  type: 'CHANGE_SEARCH_TEXT',
-  searchText: 'New new search text'
-});
+store.dispatch(changeSearchText('New new search text'));
 
-store.dispatch({
-  type: 'ADD_TODO',
-  text: 'A very new todo'
-});
+store.dispatch(changeSearchText('Super new search text'));
 
-store.dispatch({
-  type: 'ADD_TODO',
-  text: 'A super new todo'
-});
+store.dispatch(addTodo('A very new todo'));
+store.dispatch(addTodo('A super new todo'));
+store.dispatch(addTodo('A third new todo'));
 
-store.dispatch({
-  type: 'ADD_TODO',
-  text: 'A third new todo'
-});
+store.dispatch(removeTodo(1));
 
-store.dispatch({
-  type: 'REMOVE_TODO',
-  id: 1
-});
+store.dispatch(changeShowCompleted(true));
